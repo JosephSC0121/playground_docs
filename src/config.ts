1fe19a4e88fa45423fe0e451199bbae22b49e9e2
JosephSC0121 <josephsilvacasas0121@gmail.com>
2024-10-16 00:37:37 -0500

    feat: muanl de usuario

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,9 @@ import type { SocialObjects } from "@/lib/types";
 export const SITE = {
   website: "https://hyperoot.dev", // replace this with your deployed domain
   author: "HYP3R00T",
-  desc: "Documentation template using Astro and Shadcn",
-  title: "CelestialDocs",
-  ogImage: "og-image.jpg",
-  repo: "https://github.com/HYP3R00T/CelestialDocs",
+  desc: "Documentación usando Astro",
+  title: "PlaygroundJDC",
+  repo: "https://github.com/JosephSC0121"
 };
 
 export const LOCALE = {
@@ -49,98 +48,26 @@ export const docconfig = {
 export const Socials: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/HYP3R00T/",
+    href: "https://github.com/JosephSC0121",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
-    name: "Facebook",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on Facebook`,
-    active: false,
-  },
-  {
     name: "Instagram",
-    href: "https://github.com/HYP3R00T/",
+    href: "https://www.instagram.com/josephsilva_21/",
     linkTitle: `${SITE.title} on Instagram`,
     active: false,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/HYP3R00T/",
+    href: "https://www.linkedin.com/in/joseph-cristian-silva-casas/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: false,
   },
   {
     name: "Mail",
-    href: "mailto:rajesh@hyperoot.dev",
+    href: "mailto:josephsilva0121@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
-    active: true,
-  },
-  {
-    name: "Twitter",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on Twitter`,
-    active: false,
-  },
-  {
-    name: "Twitch",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on Twitch`,
-    active: false,
-  },
-  {
-    name: "YouTube",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on YouTube`,
-    active: false,
-  },
-  {
-    name: "WhatsApp",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on WhatsApp`,
-    active: false,
-  },
-  {
-    name: "Snapchat",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on Snapchat`,
-    active: false,
-  },
-  {
-    name: "Pinterest",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on Pinterest`,
-    active: false,
-  },
-  {
-    name: "Discord",
-    href: "https://discord.gg/tWZRBhaPhd",
-    linkTitle: `${SITE.title} on Discord`,
-    active: false,
-  },
-  {
-    name: "GitLab",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on GitLab`,
-    active: false,
-  },
-  {
-    name: "Reddit",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on Reddit`,
-    active: false,
-  },
-  {
-    name: "Telegram",
-    href: "https://github.com/HYP3R00T/",
-    linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "https://mastodon.social/@hyp3r00t",
-    linkTitle: `${SITE.title} on Mastodon`,
     active: true,
   },
 ];
